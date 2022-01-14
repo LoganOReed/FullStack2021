2c12e1262326151d2d2d95c5c3f247964ae5e727
orlogan <orlogan7@gmail.com>
2022-01-14 03:29:54 -0500

    Basic Phonebook Input

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleContactSubmit = (e) => {
+    e.preventDefault()
+    console.log('button clicked', e.target)
+    console.log('filtered array ', persons.find( p => p.name === newName))
+    if(persons.find( p => p.name === newName) !== undefined){
+      alert(`${newName} is already in the phonebook`)
+    }
+    else{
+      setPersons(persons.concat({name: newName}))
+    }
+  }
+
+  const handleContactChange = (e) => {
+    setNewName(e.target.value)
+    console.log('button clicked', e.target)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleContactSubmit}>
+        <div>
+          name: 
+          <input 
+            value={newName}
+            onChange={handleContactChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map( (person) => {
+        return (
+          <p key={person.name}>{person.name}</p>
+        )
+      })}
+      ...
+      <div>debug: {newName}</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
