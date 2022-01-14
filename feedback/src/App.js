4acdaa8b8c4ddf227f90dcf08bec1d65cbbc8b0a
orlogan <orlogan7@gmail.com>
2022-01-14 00:56:28 -0500

    Fixed Part1 Data Structures

--- feedback/src/App.js
+++ feedback/src/App.js
@@ -1,3 +1,5 @@
+//https://fullstackopen.com/en/part2/rendering_a_collection_modules#exercises-2-1-2-5
+
 import React, { useState } from 'react'
 
 const Header = ({ title }) => {
--- part1/src/App.js
+++ part1/src/App.js
@@ -1,85 +1,83 @@
 import React, { useState } from "react"
 
-const Header = (props) => {
+const Header = ({course}) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{course}</h1>
     </div>
   )
 }
 
-const Part = (props) => {
+const Part = ({name, exercise}) => {
 
   return (
     <div>
     <p>
-        {props.part} {props.exercise}
+        {name} {exercise}
     </p>
     </div>
   );
 }
 
-const Content = (props) => {
+const Content = ({parts}) => {
   return (
     <div>
-      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
+      {parts.map( (part) => {
+        return (<Part key={part.id} name={part.name} exercise={part.exercises} />);
+      })}
     </div>
   )
 }
 
-const Total = (props) => {
+const Total = ({parts}) => {
+  //construct an array of exercises from parts array
+  const exercises = parts.map((part) => {
+    return part.exercises;
+  })
   return (
     <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+      <p>Total: {exercises.reduce((prev, curr) => {
+        return prev + curr;
+      })}</p>
     </div>
   )
 }
 
-const AppOne = () => {
-  
-  const course = {
-    name: 'Half Stack application development',
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7
-        },
-        {
-          name: 'State of a component',
-          exercises: 14
-        }
-      ]
-  }
+const Course = ({course}) => {
 
   return (
     <div>
       <Header course={course.name} />
-      <Content  parts={course.parts} />
+      <Content parts={course.parts} />
       <Total parts={course.parts} />
     </div>
   );
 }
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
-  return (
-    <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
-        plus
-      </button>
-      <button onClick={() => setCounter(0)}>
-        zero
-      </button>
-    </div>
-  )
+  return (<Course course={course} />)
 }
 
 export default App;
